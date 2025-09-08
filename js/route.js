5a07ea5aa6e6c02731998aecda75b8d37a958034
LukasBD <jlucasBucalon@gmail.com>
2025-09-07 21:39:42 -0300

    val

--- js/route.js
+++ js/route.js
@@ -1,14 +1,5 @@
 // route.js
-// import { routes } from "./routes.js";
-
-const routes = [
-  { path: /^\/$/, page: "pages/Home/home" },
-  { path: /^\/Botoes$/, page: "pages/Buttons/buttons" },
-  { path: /^\/Fundos$/, page: "pages/Background/background" },
-  { path: /^\/Anotacoes$/, page: "pages/Note/note" },
-  { path: /^\/Sobre$/, page: "pages/About/about" },
-  { path: /^\/Contato$/, page: "pages/Contact/contact" },
-];
+import { routes } from "./main.js";
 
 const content = document.getElementById("content");
 const pageCache = {}; // cache em memória
--- js/routes.js
+++ js/routes.js
@@ -1,1 +1,9 @@
 // routes.js
+export const routes = [
+  { path: /^\/$/, page: "pages/Home/home" },
+  { path: /^\/Botoes$/, page: "pages/Buttons/buttons" },
+  { path: /^\/Fundos$/, page: "pages/Background/background" },
+  { path: /^\/Anotacoes$/, page: "pages/Note/note" },
+  { path: /^\/Sobre$/, page: "pages/About/about" },
+  { path: /^\/Contato$/, page: "pages/Contact/contact" },
+];
